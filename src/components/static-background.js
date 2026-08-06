83d4a75bffca9cf11008ce748d530b990729a278
Koushik Murali <m.koushik1995@gmail.com>
2026-08-06 21:00:07 +0530

    fix: Restore background visibility by updating z-index

--- src/components/static-background.js
+++ src/components/static-background.js
@@ -12,7 +12,7 @@ export class StaticBackground extends LitElement {
       left: 0;
       width: 100%;
       height: 100%;
-      z-index: -1;
+      z-index: 0;
       background-color: #000;
     }
     
